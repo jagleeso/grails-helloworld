62d5e6f2d53365d3b722ab0eef586f5ad105352b
James Gleeson <james@innovativemedicine.ca>
2013-05-09 16:19:55 -0400

    complete task 4): create a new view and controller method for User that lists users alternating with ROYGBIV colours

--- grails-app/controllers/helloworld/UserController.groovy
+++ grails-app/controllers/helloworld/UserController.groovy
@@ -15,6 +15,20 @@ class UserController {
         [userInstanceList: User.list(params), userInstanceTotal: User.count()]
     }
 
+    def colouredList(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        def moduloToCSSClass = [
+            0: 'red',
+            1: 'orange',
+            2: 'yellow',
+            3: 'green',
+            4: 'blue',
+            5: 'indigo',
+            6: 'violet',
+        ]
+        [userInstanceList: User.list(params), userInstanceTotal: User.count(), moduloToCSSClass: moduloToCSSClass]
+    }
+
     def create() {
         [userInstance: new User(params)]
     }
